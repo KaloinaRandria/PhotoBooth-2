c312a03134b597b1482dc364d8272330db77d969
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-06-15 18:51:54 +0300

    Update list-client.component.ts
    add calculateAge function

--- src/app/layout/home/client/list-client/list-client.component.ts
+++ src/app/layout/home/client/list-client/list-client.component.ts
@@ -31,4 +31,10 @@ export class ListClientComponent implements OnInit{
       }
     });
   }
+  calculateAge(dateOfBirth : string): number {
+    const dob = new Date(dateOfBirth);
+    const diffMs = Date.now() - dob.getTime();
+    const ageDt = new Date(diffMs);
+    return Math.abs(ageDt.getUTCFullYear() - 1970);
+  }
 }
