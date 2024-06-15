f8ec69abacd592f16990583111834aac82ffa8e8
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-06-15 18:47:03 +0300

    client list ts

--- src/app/layout/home/client/list-client/list-client.component.ts
+++ src/app/layout/home/client/list-client/list-client.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from "../../../../service/client/client.service";
+import {Display} from "../../../../class/util/display";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-list-client',
   templateUrl: './list-client.component.html',
   styleUrl: './list-client.component.css'
 })
-export class ListClientComponent {
-
+export class ListClientComponent implements OnInit{
+  client : any[] = [];
+  ngOnInit() {
+    this.getAllClient();
+  }
+  constructor(private clientService : ClientService , private snackBar : MatSnackBar) {
+  }
+  getAllClient():void{
+    const api = '/client/all';
+    this.clientService.getAll(api).subscribe({
+      next:(response : any) =>{
+        if (response.success) {
+          this.client = response.data;
+          console.log(this.client);
+        } else {
+          Display.alert(this.snackBar,(response.message),"close",6000);
+        }
+      },
+      error:(exception) => {
+        Display.alert(this.snackBar , (exception.error.message),"close",6000);
+      }
+    });
+  }
 }
