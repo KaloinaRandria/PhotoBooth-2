a16178592235d9c5b4045c114cb54abc936d80db
tonnyBryan <126952328+tonnyBryan@users.noreply.github.com>
2024-06-17 15:44:44 +0300

    liste theme
    integr liste theme

--- src/app/layout/home/theme/list-theme/list-theme.component.ts
+++ src/app/layout/home/theme/list-theme/list-theme.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Constants } from '../../../../class/util/constants';
 
 @Component({
   selector: 'app-list-theme',
   templateUrl: './list-theme.component.html',
   styleUrl: './list-theme.component.css'
 })
-export class ListThemeComponent {
+export class ListThemeComponent implements OnInit {
 
+  themeList: any[] = []
+  ngOnInit(): void {
+      
+  }
+
+  constructor(private http: HttpClient) {
+    this.getAllTheme();
+  }
+
+  getAllTheme() {
+    this.http.get(Constants.BACK_URL + '/theme/all').subscribe({
+      next:(valiny: any)=> {
+        this.themeList = valiny.data;
+      },
+      error:(err) => {
+        console.error(err);
+      }
+    });
+  }
 }
