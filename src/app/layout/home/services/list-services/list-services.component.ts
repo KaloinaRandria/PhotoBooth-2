478b38c5837f425465378338bb45057ac6edbf9f
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-06-22 12:21:26 +0300

    liste services

--- src/app/layout/home/services/list-services/list-services.component.ts
+++ src/app/layout/home/services/list-services/list-services.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServicesBoothService } from '../../../../service/services/services-booth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Display } from '../../../../class/util/display';
@@ -8,22 +8,27 @@ import { Display } from '../../../../class/util/display';
   templateUrl: './list-services.component.html',
   styleUrl: './list-services.component.css'
 })
-export class ListServicesComponent {
-
+export class ListServicesComponent implements OnInit{
+  services : any[] = [];
   constructor(private service : ServicesBoothService , private snackBar : MatSnackBar) {
   }
 
+  ngOnInit() {
+    this.getAllServices();
+  }
   getAllServices() {
-    const api = 'service/all';
+    const api = '/service/all';
     this.service.getAll(api).subscribe({
        next:(response : any) =>{
         if (response.success) {
-          
+          this.services = response.data;
+          console.log(this.services);
         } else {
           Display.alert(this.snackBar,(response.message),"close",6000);
         }
       },
       error:(exception) => {
+           console.log(exception);
         Display.alert(this.snackBar , (exception.error.message),"close",6000);
       }
     });
