239f743bd4b75964b6f8ede8a650dceb4648f507
MaheryJeremie <mramahay2103@gmail.com>
2024-06-22 13:59:00 +0300

    insert depense

--- src/app/layout/home/record/insert-record/insert-record.component.ts
+++ src/app/layout/home/record/insert-record/insert-record.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {RecordService} from "../../../../service/record/record.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Display} from "../../../../class/util/display";
 
 @Component({
   selector: 'app-insert-record',
@@ -6,7 +10,36 @@ import { Component } from '@angular/core';
   styleUrl: './insert-record.component.css'
 })
 export class InsertRecordComponent {
+  form:FormGroup;
+  constructor(private recordService:RecordService,private formBuilder:FormBuilder,private snackBar:MatSnackBar) {
+    this.form=formBuilder.group({
+        label:['',Validators.required],
+        amount:['',[Validators.required,Validators.min(0)]],
+        date:['',Validators.required]
+    });
+  }
   submit() {
+    if(this.form.valid){
+      const data={
+        libele:this.form.get('label')?.value,
+        montant:this.form.get('amount')?.value,
+        date_insertion:this.form.get('date')?.value
+      }
+      console.log(data);
+      this.recordService.save(data,'/depense/save').subscribe({
+        next: () => {
+          Display.alert(this.snackBar, "Envoyé avec succès", "Fermer", 3000, "success-snackbar");
+        },
+        error: (exception) => {
+          Display.alert(this.snackBar, exception.error.message, "Fermer", 6000);
+        }
+      });
+    }
+    else{
+      Object.values(this.form.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
 
   }
 }
