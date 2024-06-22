5c8f974b2aa6b67d3b63652ba3bb898bd071c32a
MaheryJeremie <mramahay2103@gmail.com>
2024-06-22 16:02:18 +0300

    liste depenses
    Reste a faire:filtre ,delete,modif

--- src/app/layout/home/record/list-record/list-record.component.ts
+++ src/app/layout/home/record/list-record/list-record.component.ts
@@ -1,44 +1,104 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import { PopUpEditRecordComponent } from './pop-up-edit-record/pop-up-edit-record.component';
+import {RecordService} from "../../../../service/record/record.service";
+import {Constants} from "../../../../class/util/constants";
+import {Display} from "../../../../class/util/display";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-list-record',
   templateUrl: './list-record.component.html',
   styleUrl: './list-record.component.css'
 })
-export class ListRecordComponent {
-  insertedReservations = [
-    { room: 'Room 1', startTime: '09:00', endTime: '11:00', amount: '$100' },
-    { room: 'Room 2', startTime: '13:00', endTime: '15:00', amount: '$150' },
-    // Autres réservations insérées...
-  ];
+export class ListRecordComponent implements OnInit{
+  records:any[]=[]
+  initialRecords:any[]=[]
+  filter:any={
+    label:'',
+    coutmin:'',
+    coutmax:'',
+    datemin:'',
+    datemax:''
+  };
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog,private recordService:RecordService,private snackbar:MatSnackBar) {
   }
-  popUp() {
+  popUp(record:any) {
 
     const dialogRef = this.dialog.open(PopUpEditRecordComponent, {
-      data: {}
+      data: {record}
     });
 
   }
-
-  filter() {
-    // Logique pour filtrer les réservations en fonction des critères spécifiés
-    console.log('Filtering reservations...');
-    // Implémentez votre propre logique de filtrage ici
+  ngOnInit() {
+    this.loadRecords();
+  }
+  loadRecords() {
+    this.recordService.getAll('/depense/all').subscribe({
+      next:(valiny: any)=> {
+        this.records = valiny.data;
+        this.initialRecords = valiny.data;
+      },
+      error:(err) => {
+        console.error(err);
+      }
+    });
+  }
+  delete(record:any){
+    const url='/materiel/delete/'+record.idDepense;
+    this.recordService.delete(url).subscribe(
+      (response:any) =>{
+        if(response.success){
+          const index = this.records.findIndex(ro => ro.id_categorie_theme === record.idDepense);
+          Display.alert(this.snackbar,"Deleted succesfully","close",3000,"succes-snackbar");
+          this.records.splice(index,1);
+        } else{
+          Display.alert(this.snackbar,'Failed to delete material',"close",6000);
+        }
+      },
+      (error)=>{
+        console.error(error);
+        Display.alert(this.snackbar,error.error.message,"close",6000);
+      }
+    );
   }
 
-  editReservation(reservation: any) {
-    // Logique pour l'édition de la réservation
-    console.log('Editing reservation:', reservation);
-    // Implémentez votre propre logique d'édition ici
+  filterRecord(
+    recordList: any[],
+    filter: any
+  ): any[] {
+    return recordList.filter(record => {
+
+      const matcheslibele = filter.label && filter.label !== '' ? record.libele.includes(filter.label) : true;
+      const matchesCoutMin = filter.coutmin && filter.coutmin !== '' ? record.montant >= Number(filter.coutmin) : true;
+      const matchesCoutMax = filter.coutmax && filter.coutmax !== '' ? record.montant <= Number(filter.coutmax) : true;
+      let matchesStartDate = true;
+      let matchesEndDate = true;
+
+      if (filter.startDate && filter.startDate !== '') {
+        matchesStartDate = new Date(record.date_insertion) >= new Date(filter.minDate);
+      }
+
+      if (filter.endDate && filter.endDate !== '') {
+        matchesEndDate = new Date(record.date_insertion) <= new Date(filter.maxDate);
+      }
+      return (
+        matcheslibele &&
+        matchesCoutMin &&
+        matchesCoutMax &&
+        matchesStartDate &&
+        matchesEndDate
+      );
+    });
   }
 
-  deleteReservation(reservation: any) {
-    // Logique pour la suppression de la réservation
-    console.log('Deleting reservation:', reservation);
-    // Implémentez votre propre logique de suppression ici
+  filterFunc() {
+    const filterTab = this.filterRecord(this.initialRecords, this.filter);
+    this.records = filterTab;
+  }
+
+  initial() {
+    this.records = this.initialRecords;
   }
 }
