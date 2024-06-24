d1289cd13a576aedb3a40ae62304ab3a1eee47c1
tonnyBryan <126952328+tonnyBryan@users.noreply.github.com>
2024-06-24 15:55:03 +0300

    comm
    comm

--- src/app/layout/home/client/statistics-client/statistics-client.component.ts
+++ src/app/layout/home/client/statistics-client/statistics-client.component.ts
@@ -1,5 +1,9 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import {HttpClient} from "@angular/common/http";
+import {Constants} from "../../../../class/util/constants";
+import {map, Observable, startWith} from "rxjs";
+import {FormControl} from "@angular/forms";
 Chart.register(...registerables);
 
 @Component({
@@ -7,6 +11,47 @@ Chart.register(...registerables);
   templateUrl: './statistics-client.component.html',
   styleUrl: './statistics-client.component.css'
 })
-export class StatisticsClientComponent {
+export class StatisticsClientComponent implements OnInit {
+  filteredOptions: Observable<any[]> | undefined;
+  searchControl = new FormControl();
+  options: any[] = [];
 
+  constructor(private http : HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getAllClient();
+    this.filteredOptions = this.searchControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filterOptions(value))
+    );
+  }
+
+  private _filterOptions(value: string): any[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.viewValue.toLowerCase().includes(filterValue));
+  }
+
+  createOptions(data: any) {
+    const options = [];
+    for (let i = 0; i < data.length; i++) {
+      options.push({ value: data[i].id_client, viewValue: data[i].nom + ' ' + data[i].prenom });
+    }
+    return options;
+  }
+
+  getAllClient() {
+    this.http.get(Constants.BACK_URL + '/client/all').subscribe({
+      next: (valiny: any) => {
+        this.options = this.createOptions(valiny.data);
+        this.filteredOptions = this.searchControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filterOptions(value))
+        );
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
