15bd317b91396b15d58a43864256c3e470cce1e8
tonnyBryan <126952328+tonnyBryan@users.noreply.github.com>
2024-06-26 13:24:25 +0300

    commit
    commit

--- src/app/layout/home/revenue/profit-revenue/profit-revenue.component.ts
+++ src/app/layout/home/revenue/profit-revenue/profit-revenue.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Constants} from "../../../../class/util/constants";
+import {Display} from "../../../../class/util/display";
+import {HttpClient} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-profit-revenue',
   templateUrl: './profit-revenue.component.html',
   styleUrl: './profit-revenue.component.css'
 })
-export class ProfitRevenueComponent {
+export class ProfitRevenueComponent implements OnInit {
+  showEstimation = false;
+  categories = ['Category A', 'Category B', 'Category C']; // Exemple de
 
+  result: any;
+  loading: boolean = true;
+
+  prevoirMoisProchain() {
+    this.predict();
+  }
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+  }
+
+  ngOnInit() {
+  }
+
+  predict() {
+    this.loading = true;
+    this.http.get(Constants.BACK_URL + '/predict/nextMonth').subscribe({
+      next:(response:any) => {
+        console.log(response);
+        this.result = response.data;
+        this.loading = false;
+      },
+      error:(exception) => {
+        Display.alert(this.snackBar,(exception.error.message),"close",6000);
+        console.error(exception);
+        this.loading = false;
+      }
+    });
+  }
 }
